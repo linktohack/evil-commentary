f93f2a2741f1c5a4281cac47ae84d08f6896a0ca
Quang-Linh LE <linktohack@gmail.com>
2015-10-27 17:36:00 +0100

    Bump to v1.2.0

--- evil-commentary.el
+++ evil-commentary.el
@@ -4,7 +4,7 @@
 
 ;; Author: Quang Linh LE <linktohack@gmail.com>
 ;; URL: http://github.com/linktohack/evil-commentary
-;; Version: 1.1.1
+;; Version: 1.2.0
 ;; Keywords: evil comment commentary evil-commentary
 ;; Package-Requires: ((evil "1.0.0"))
 
