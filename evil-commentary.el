ab9e7bf61324ecf24b3611b9c4951785a83b7ab8
Quang-Linh LE <linktohack@gmail.com>
2016-02-21 21:24:20 +0100

    Extract integration part

--- evil-commentary.el
+++ evil-commentary.el
@@ -43,6 +43,7 @@
 ;;; Code:
 
 (require 'evil)
+(require 'evil-commentary-integration)
 
 (defgroup evil-commentary nil
   "Comment stuff out"
@@ -113,40 +114,6 @@ parameter."
             (evil-define-key 'normal map "gy" 'evil-commentary-yank)
             (define-key map (kbd "s-/") 'evil-commentary-line)
             map))
-
-(defmacro evil-commentary/org-babel-do-in-edit-buffer (beg end &rest body)
-  "Do `org-babel-do-in-edit-buffer' and restore view.
-
-Return the same value as `org-babel-do-in-edit-buffer'. Save top
-line of current window and restore it if sucess."
-  (declare (indent defun))
-  `(when (and (fboundp 'org-babel-do-in-edit-buffer)
-              (org-in-src-block-p t))
-     (let ((top-line (line-number-at-pos (window-start)))
-           (current-point (point))
-           found)
-       (push-mark ,beg)
-       (goto-char ,end)
-       (setq mark-active t)
-       (setq found (eval '(org-babel-do-in-edit-buffer
-                             ,@body)))
-       (pop-mark)
-       (if (not found)
-           (goto-char current-point)
-         (save-excursion
-           (scroll-up 1)              ; stupid fix
-           (goto-char (point-min))
-           (forward-line (1- top-line))
-           (recenter 0)))
-       found)))
-
-;;;###autoload
-(defun evil-commentary/org-comment-or-uncomment-region (beg end)
-  "Comment function for `org-mode'."
-  (interactive "r")
-  (unless (evil-commentary/org-babel-do-in-edit-buffer beg end
-            (call-interactively 'evil-commentary))
-    (comment-or-uncomment-region beg end)))
 
 (provide 'evil-commentary)
 
